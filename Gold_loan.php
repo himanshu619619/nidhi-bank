7589cb7d92faf2cfac6f5d946e1cd14fff63c8c3
himanshu619619 <himanshuthakur619@gmail.com>
2020-03-02 18:59:58 +0530

    new pages of gold loan

---
+++ Gold_loan.php
@@ -0,0 +1,106 @@
+<?php include 'header.php'; ?>
+<div class="container ">
+    <div class="row" style="margin: 5% auto;">
+
+   <div class="col-md-12">
+   <h1> GOLD <strong>LOAN</strong></h1>
+<p>Get your idle gold to work for you with an unbeatable loan</p>
+
+<p>Make the most of your idle gold with Bfss attractive gold loans. Get a loan in 45 minutes over the
+counter with minimal documentation and transparent charges.</p>
+<p>The funds from gold loans can be used for weddings, education, business expansion or any other
+similar purpose. With minimal documentation and secure storage, our gold loans are a simple
+funding option to meet your needs. Pay only the interest amount every month and enjoy low
+outgoings.</p>
+
+<h1>FEATURES</h1>
+<strong>Loan amount</strong>
+<p>Get loans starting at ₹ 25,000 (Minimum loan amount of ₹ 10000/- available in rural markets)</p>
+<STRONG>Interest rates</strong>
+<p>
+BFSS Gold Loan comes with competitive interest rates on Term Loan, Overdraft and EMI based
+loan. Repay your loan in easy lower EMIs, over a tenor of your choice.</p>
+<strong>Tenure</strong>
+<p>
+Gold Loans are available for tenures starting from 6 months up to 36 months.</p>
+<strong>Repayment</strong>
+<p>
+Repay only the interest on the loan every month</p>
+<p>Enjoy monthly outflow as low as ₹ 1,000 per lakh (based on an indicative rate of 1% pm)</p>
+<strong>Processing and approval</strong>
+Fastest Tounround
+<p>Enjoy simple documentation and speedy disbursals</p>
+<p>Pay no hidden charges and experience a completely transparent process</p>
+<p>gold loan was disbursed super quick - in 45 minutes</p>
+
+
+<h1>DOCUMENTATION</h1>
+<p>Submit any two of the following:</p>
+<li>Passport (Not expired)</li>
+<li>Driving license (Not expired)</li>
+<li>Voters’ ID Card</li>
+<li>Aadhaar Card issued by UIDAI</li>
+<li>PAN (Permanent Account Number) Card (along with any of above mentioned documents) or
+Form 60</li>
+<li>One Passport Size Photograph (mandatory)</li>
+
+<p>Rates offered to customer during the period of July&#39;19 to Sep&#39;19*.</p>
+<table class="table table-bordered text-center">
+<thead class="thead-dark text-center" style="background:black; color:white;">
+<tr>
+<th scope="col" rowspan="2" >  Product Group  </th>
+<th scope="col" colspan="3" class="text-center">    Bank IRR</th>
+</tr>
+<tr>
+      
+       
+        <th scope="col"> Min </th>
+        <th scope="col"> Max </th>
+        <th scope="col"> Avg</th>
+</tr>
+</thead>
+<tr>
+        <td>GOLD LOAN </td>
+        <td>10.00%</td> 
+        <td>16.15% </td>
+        <td>11.69%</td>
+</tr>
+
+</table>
+Annual Percentage Rate offered to customer during the period of July&#39;19 to Sep&#39;19*.
+
+<table class="table table-bordered text-center">
+<thead class="thead-dark text-center" style="background:black; color:white;">
+
+<tr>
+      
+       
+        <th scope="col"> APR Min</th>
+        <th scope="col"> APR Max</th>
+        <th scope="col"> APR Avg</th>
+</tr>
+</thead>
+<tr>
+       
+        <td>10.01%</td> 
+        <td> 25.58%  </td>
+        <td>12.07%</td>
+</tr>
+
+</table>
+     
+
+
+<p>
+<strong>Note:</strong> GST and other Government taxes, levies etc. applicable as per prevailing rate will be charged
+over and above the Fees and Charges</p>
+<p>Loan at the sole discretion of HDFC Bank Ltd.</p>
+<p>*T&amp;C- Loan Approval and ROI at the sole</p>
+
+</div>
+    </div>
+    </div>
+ 
+
+
+<?php include 'footer.php'; ?>
--- ujwal_bhavishya_yojana.php
+++ ujwal_bhavishya_yojana.php
@@ -3,7 +3,39 @@
 <div class="container ">
     <div class="row" style="margin: 5% auto;">
 
-	
+	<h1>Ujwal <strong>Bhavishya Yojana</strong></h1>
+<p>Ujwal bhavishya Yojana account is a saving scheme launch by bfss register in
+government of india.</a>
+<p>
+It aims mainly for the higher education and marriage for the girl and boy child
+The ujwal bhavishya Yojana is related to the girl or boy child scheme under this we
+help out the financial burden of higher education and marriage for both. The scheme
+has well been accepted by the masses in wake of the financial security so they can
+achieve their dreams. it would provide to the girl or boy both as well as their parents
+and guardians.
+</p>
+
+
+<h2>Features</h2>
+
+<li>Cash</li>
+<li>Cheque</li>
+<li>Transfer/ online transfers through internet Banking</li>
+<li>To promote the welfare of both Girl n boy Child</li>
+<li>A natural/ legal guardian on behalf of a girl/boy child</li>
+<li>Min.1500 of initial deposit with multiple of one Fifty rupees.</li>
+<li>Tenure of the Deposit is 21 years from the date of opening of the account</li>
+<li>Maximum period upto which deposits can be made 15 years from the date of opening of the
+account.</li>
+<li>As notified by the bfss, compounded annually with option for monthly interest pay-outs to be
+calculated on balance in completed thousands.( Current rate 8.45%)</li>
+<li>Irregular Payment/ Revival of account by payment of penalty of Rs.100 per month along with the
+minimum specified amount per year</li>
+
+<h1>Terms And <strong>Conditions</strong></h1>
+<stong>Premature Closure</strong>
+<li>Allowed in the event of death of the depositor or in cases of extreme compassionate grounds
+such as medical support</li>
 
 
     </div>
